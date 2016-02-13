6f5ffa435c1163661bc631a6152df98b939536c9
bibi21000 <bibi21000@gmail.com>
2016-02-13 19:17:32 +0100

    Updates for raspberry

--- tests/test_server.py
+++ tests/test_server.py
@@ -64,13 +64,4 @@ class TestPiSerser(JNTTServer, JNTTServerCommon):
     server_class = PiServer
     server_conf = "tests/data/janitoo_raspberry_i2c.conf"
 
-    def test_110_request_system_values(self):
-        self.start()
-        nodeHADD=HADD%(139,0)
-        self.assertHeartbeatNode(hadd=nodeHADD)
-        self.assertNodeRequest(cmd_class=COMMAND_DISCOVERY, uuid='request_info_nodes', node_hadd=nodeHADD, client_hadd=HADD%(9999,0))
-        self.assertBroadcastRequest(cmd_class=COMMAND_DISCOVERY, uuid='request_info_nodes', client_hadd=HADD%(9999,0))
-        self.stop()
-
-
 
