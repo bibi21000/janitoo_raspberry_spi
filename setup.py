c23186286bf5b472fb299386be2bd9081ecbe3c5
bibi21000 <bibi21000@gmail.com>
2016-04-21 21:39:58 +0200

    Update deps

--- setup.py
+++ setup.py
@@ -96,12 +96,14 @@ setup(
                      'janitoo',
                      'janitoo_raspberry',
                      'Adafruit-GPIO',
+                     'Adafruit-PureIO',
                     ],
     dependency_links = [
       'https://github.com/bibi21000/janitoo/archive/master.zip#egg=janitoo',
       'https://github.com/bibi21000/janitoo_raspberry/archive/master.zip#egg=janitoo_raspberry',
       'https://github.com/bibi21000/janitoo_raspberry_gpio/archive/master.zip#egg=janitoo_raspberry_gpio',
       'https://github.com/adafruit/Adafruit_Python_GPIO/archive/master.zip#egg=Adafruit-GPIO',
+      'https://github.com/adafruit/Adafruit_Python_PureIO/archive/master.zip#egg=Adafruit-PureIO',
     ],
     entry_points = janitoo_entry_points,
 )
