ebad8c4141b1954b6d6188d669a8b64784ade639
bibi21000 <bibi21000@gmail.com>
2016-05-07 02:14:24 +0200

    Massive updates for threads

--- src/janitoo_raspberry_spi/thread_spi.py
+++ src/janitoo_raspberry_spi/thread_spi.py
@@ -49,8 +49,8 @@ assert(COMMAND_DESC[COMMAND_CONTROLLER] == 'COMMAND_CONTROLLER')
 
 OID = 'rpispi'
 
-def make_thread(options):
-    if get_option_autostart(options, OID) == True:
+def make_thread(options, force=False):
+    if get_option_autostart(options, OID) == True or force:
         return RpiSPIThread(options)
     else:
         return None
